d18663e0ddd7cfb9956b94204e194dc69801d768
John Ernsthausen <john@JohnErnsthausen.com>
2021-07-03 17:06:53 -0400

    Pathname to repository requires git not checkout in circleci

--- test/ioTests.cpp
+++ test/ioTests.cpp
@@ -10,36 +10,29 @@
 
 #include "io.hpp"
 
-#define DOUBLE_NEAR(x) DoubleNear((x), 4.0 * DBL_EPSILON)
-
 using namespace testing;
-using namespace std;
 
-class TestThatIO : public Test
-{
-};
-
-TEST_F(TestThatIO, FindsROCSubdirectoryInCurrentWorkingDirectory)
+TEST(TestThatIO, FindsROCSubdirectoryInCurrentWorkingDirectory)
 {
   std::cout << std::filesystem::current_path() << "\n";
-  string roc{"roc"};
+  std::string roc{"roc"};
   ASSERT_NO_THROW(cwd_path_to(roc));
 }
 
-TEST_F(
+TEST(
     TestThatIO,
     ThrowsExceptionWhenItDoesNotFindGivenSubdirectoryInCurrentWorkingDirectory)
 {
-  string roc{"foo"};
+  std::string roc{"foo"};
   ASSERT_THROW(cwd_path_to(roc), std::exception);
 }
 
-TEST_F(TestThatIO, ReadsData)
+TEST(TestThatIO, ReadsData)
 {
-  filesystem::path dn;
+  std::filesystem::path dn;
   // try
   //{
-  string roc{"roc"};
+  std::string roc{"roc"};
   dn = cwd_path_to(roc);
   //}
   // catch (const std::exception& e)
@@ -47,23 +40,23 @@ TEST_F(TestThatIO, ReadsData)
   //  cout << e.what() << '\n';
   //}
 
-  string filename{dn / "test" / "coeff.txt"};
+  std::string filename{dn / "test" / "coeff.txt"};
   // cout << filename << "\n";
-  EXPECT_THAT(filename, EndsWith(string("roc/test/coeff.txt")));
+  EXPECT_THAT(filename, EndsWith(std::string("roc/test/coeff.txt")));
 
-  ifstream input{filename};
+  std::ifstream input{filename};
   // if (!input)
   //{
   //  perror("Error opening input file");
   //  // return -1;
   //}
 
-  vector<double> coeffs;
+  std::vector<double> coeffs;
   double time{0};
   double scale{0};
-  string s;
-  cout.precision(16);
-  cout << scientific;
+  std::string s;
+  std::cout.precision(16);
+  std::cout << std::scientific;
   int i{0};
   while (getline(input, s))
   {
@@ -78,10 +71,10 @@ TEST_F(TestThatIO, ReadsData)
     // For testing exercise output operator << on first line read
     if (i == 0)
     {
-      cout << coeffs;
-      cout << "T     = " << time << '\n';
-      cout << "Scale = " << scale << '\n';
-      cout << "Size  = " << coeffs.size() << '\n';
+      std::cout << coeffs;
+      std::cout << "T     = " << time << '\n';
+      std::cout << "Scale = " << scale << '\n';
+      std::cout << "Size  = " << coeffs.size() << '\n';
       i++;
     }
     EXPECT_THAT(time, Ge(0.0));
